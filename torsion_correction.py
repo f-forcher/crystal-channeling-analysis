74bbd97ae093e6894cbb99c92eb4d962bf7b6cfa
fforcher <francesco.forcher@gmail.com>
2017-09-18 16:14:08 +0200

    Aggiunto init_Scan a thetax. Da fare il plot dell'efficienza adesso

--- torsion_correction.py
+++ torsion_correction.py
@@ -22,7 +22,7 @@ import editable_input as ei # My script for editable text input
 # xmax	0.475
 #
 
-
+file_name = sys.argv[1]
 
 if os.path.isfile('crystal_analysis_parameters.csv'):
     parameters_table = pd.read_csv("crystal_analysis_parameters.csv", sep="\t", index_col=0)
@@ -30,9 +30,9 @@ else: #
     raise FileNotFoundError("[ERROR]: File crystal_analysis_parameters.csv not "
                             "found. Create it with save_as_hdf.py")
 
-cut_left = parameters_table.loc['xmin']
-cut_right = parameters_table.loc['xmax']
-init_scan = parameters_table.loc['init_scan']
+cut_left = float(parameters_table.loc['xmin'])
+cut_right = float(parameters_table.loc['xmax'])
+init_scan = float(parameters_table.loc['init_scan'])
 #################
 
 
@@ -52,10 +52,12 @@ init_scan = parameters_table.loc['init_scan']
 #        'Tracks_d0Err_x', 'Tracks_d0Err_y', 'Tracks_chi2_x',
 #        'Tracks_chi2_y', 'SingleTrack', 'MultiHit'
 chunksize = 2000000
-interesting_columns = ["Tracks_d0_x", "Tracks_thetaOut_x", "Tracks_thetaIn_x"]
+interesting_columns = ["Tracks_d0_y", "Tracks_thetaOut_x", "Tracks_thetaIn_x"]
+# Important to remember that the columns need to be indexed with
+# data_columns=[...] when .hdf is created, to be able to use where on them
 cuts_and_selections = ["SingleTrack == 1", "Tracks_d0_x > cut_left", "Tracks_d0_x < cut_right"]
 
-evts = pd.read_hdf(file_name, chunksize = chunksize, columns=interesting_columns, where="SingleTrack == 1")
+evts = pd.read_hdf(file_name, chunksize = chunksize, columns=interesting_columns, where=cuts_and_selections)
 
 loaded_rows = 0
 events = pd.DataFrame(columns=interesting_columns)
@@ -66,5 +68,32 @@ for df in evts:
     df.info()
     events = events.append(df,ignore_index=True) # join inner maybe unnecessary here
     # break; # Uncomment to get only the first chunk
-print("[LOG]: Loaded data!")
+print("\n[LOG]: Loaded data!\n")
+events.info()
+#################
+
+events["Tracks_thetaIn_x_CORRECTED"] = events.loc[:,"Tracks_thetaIn_x"].values + \
+                                       init_scan*1e-6
+
+events.info()
+
+################# BIN THE DATA
+# low_xrange_edge = -4
+# high_xrange_edge = 4
+# nbins = 12
+#
+# # num=6 because we want nbins=5 bins and so we need num = nbins + 1 = 6 bin "borders"
+# xbins = np.linspace(low_range_edge,high_range_edge,num=nbins+1)
+# ybins = np.linspace(low_range_edge,high_range_edge,num=nbins + 1)
+# delta_xbins = xbins[1] - xbins[0]
+# delta_ybins = ybins[1] - ybins[0]
+#
+# # bin centers, basically we create a list which is the bin edges above, moved forward by half bin width, and then
+# # we remove the last one with [:-1] because the last one is half bin after the last edge at high_range_edge
+# xbincenters = np.linspace(low_range_edge + delta_xbins/2, high_range_edge + delta_xbins/2, num=nbins + 1)[:-1]
+# ybincenters = np.linspace(low_range_edge + delta_ybins/2, high_range_edge + delta_ybins/2,num=nbins + 1)[:-1]
+#
+# gruppi = df.groupby([pd.cut(df.X, xbins, labels = xbincenters),
+#                      pd.cut(df.Y, ybins, labels = ybincenters)])
+
 #################
